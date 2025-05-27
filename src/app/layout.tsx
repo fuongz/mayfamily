2d2e6feed73ea77521931c7d84af9824edeb65a6
fuongz <hi@phuongphung.com>
2025-05-27 12:02:42 +0700

    ✨ feat: add umami tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,45 @@
-import type { Metadata } from "next";
-import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
+import type { Metadata } from 'next'
+import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core'
 
-import "./globals.css";
+import './globals.css'
 
-import "@mantine/core/styles.css";
-import "@mantine/carousel/styles.css";
+import '@mantine/core/styles.css'
+import '@mantine/carousel/styles.css'
 
-import Providers from "./providers";
-import { League_Gothic } from "next/font/google";
-import { AudioPlayer } from "@/components/layout/audio-player/AudioPlayer";
+import Providers from './providers'
+import { League_Gothic } from 'next/font/google'
+import { AudioPlayer } from '@/components/layout/audio-player/AudioPlayer'
+import Script from 'next/script'
+import { URL } from '@/config/url'
 
 export const metadata: Metadata = {
-  title: "Trinh & Phương",
-  description: "Trang web gia đình bé nhỏ của chúng mình!",
-};
+  title: 'Trinh & Phương',
+  description: 'Trang web gia đình bé nhỏ của chúng mình!',
+}
 
 const fontTitle = League_Gothic({
-  variable: "--font-title",
-  subsets: ["vietnamese"],
-  weight: ["400"],
-});
+  variable: '--font-title',
+  subsets: ['vietnamese'],
+  weight: ['400'],
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Trinh & Phuong" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <link rel="manifest" href="/site.webmanifest" />
+        <Script src="https://cloud.umami.is/script.js" data-website-id={URL.WEBSITE_ID} />
       </head>
       <body className={fontTitle.variable}>
         <Providers>
@@ -57,5 +48,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  );
+  )
 }
--- src/config/url.ts
+++ src/config/url.ts
@@ -1,3 +1,4 @@
 export const URL = {
   CDN_URL: process.env.NEXT_PUBLIC_CDN_URL,
-};
+  WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+}
